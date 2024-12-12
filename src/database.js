c050a68f6bdddfeee134a2a610174b1755779191
Ludovic2711 <rendonrestrepol@gmail.com>
2024-12-12 13:18:07 -0500

    Cambio de Base de datos

--- src/database.js
+++ src/database.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql2')
+const { Pool } = require('pg');
 
-const mysqlConnection = mysql.createConnection({
-    host: 'bu8jsnccetgp37myqw1g-mysql.services.clever-cloud.com',
-    user: 'u6pywbvgcv04n18v',
-    password: 'GqL0PP0CfPkrjlmef2Oo',
-    database: 'bu8jsnccetgp37myqw1g'
+//const mysqlConnection = new Pool({
+//    host: 'dpg-ctdgpmtumphs73837s0g-a',
+//    user: 'movieshop_user',
+//    password: 'zP3QitlzPNpByoqWO3FMSSzFo1KSISrl',
+//    database: 'movieshop',
+//  port:5432,
+//})
+
+const mysqlConnection = new Pool({
+connectionString: 'postgresql://movieshop_user:zP3QitlzPNpByoqWO3FMSSzFo1KSISrl@dpg-ctdgpmtumphs73837s0g-a.oregon-postgres.render.com:5432/movieshop',
+      ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 mysqlConnection.connect(function (err) {
